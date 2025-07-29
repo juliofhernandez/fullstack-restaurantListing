68ae3a958be1b4b876f42055174febb2ce7732ac
Canchala Hernandez <julio.canchalahernandez@capgemini.com>
2025-07-29 11:32:34 +0200

    init

---
+++ src/main/java/com/juliofhernandez/fullstackrestaurantlisting/FullstackRestaurantListingApplication.java
@@ -0,0 +1,13 @@
+package com.juliofhernandez.fullstackrestaurantlisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackRestaurantListingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackRestaurantListingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juliofhernandez/fullstackrestaurantlisting/FullstackRestaurantListingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliofhernandez.fullstackrestaurantlisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackRestaurantListingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
